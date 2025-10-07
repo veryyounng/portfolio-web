3b1bd29ee9a2582f90f50424c602011b51f4ed97
소영 <veryyoung811@gmail.com>
2025-10-07 18:00:51 +0900

    스크롤 active, 애니메이션, 텍스트 타이핑 효과 적용

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ function toggleMenu() {
   const $navMenu = document.getElementById('nav__menu');
   $navMenu.classList.toggle('show');
 }
+
+function handleFloatingButton() {
+  const $floatingButton = document.getElementById('floating-button');
+  $floatingButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      // behavior: 'smooth',
+    });
+  });
+}
+
 function init() {
   const $navToggle = document.getElementById('nav-toggle');
   $navToggle.addEventListener('click', () => {
@@ -11,6 +22,60 @@ function init() {
 
   const $navLinkList = document.querySelectorAll('.nav__link ');
   $navLinkList.forEach((el) => el.addEventListener('click', toggleMenu));
+
+  handleFloatingButton();
 }
 
 init();
+
+const options = {
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const sectionId = entry.target.id;
+    if (entry.isIntersecting) {
+      document
+        .querySelector(`.nav__link[href*=${sectionId}]`)
+        .classList.add('active-link');
+
+      const $items = document.querySelectorAll(
+        `.nav__link:not([href*=${sectionId}])`,
+      );
+      $items.forEach((el) => el.classList.remove('active-link'));
+    }
+  });
+}, options);
+
+const $sectionList = document.querySelectorAll('.section');
+$sectionList.forEach((el) => observer.observe(el));
+// observer.observe($workSection);
+// observer.observe($workSection);
+// observer.observe($workSection);
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 200,
+});
+scrollReveal.reveal('.home__data, .about__img, .skills__text');
+scrollReveal.reveal('.home__img, .about__data, .skills__img', { delay: 400 });
+scrollReveal.reveal('.skills__data, .work__link, .contact__input', {
+  interval: 200,
+});
+
+const typeit = new TypeIt('#typeit', {
+  speed: 70,
+  startDelay: 1300,
+  waitUntilVisible: true,
+});
+
+typeit
+  .type('안녕하세요!<br/>')
+  .type('안녕하세요!<br/>')
+  .type('<strong class="home__title-color">백엔드 개발자  </strong><br/>')
+  .type('<strong class="home__title-color">Fairy</strong>', { delay: 300 })
+  .delete(6, { delay: 300 })
+  .type('<strong class="home__title-color">최소영</strong>입니다!')
+  .go();
