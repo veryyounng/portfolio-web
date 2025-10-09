123f6ad13f0598e187cf8335ce2f108ac139298a
소영 <veryyoung811@gmail.com>
2025-10-09 18:01:55 +0900

    사진, 자기소개, 포트폴리오 링크 업데이트

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ const typeit = new TypeIt('#typeit', {
 typeit
   .type('안녕하세요!<br/>')
   .type('<strong class="home__title-color">백엔드 개발자  </strong><br/>')
-  .type('<strong class="home__title-color">Fairy</strong>', { delay: 300 })
-  .delete(5, { delay: 300 })
+  // .type('<strong class="home__title-color">Fairy</strong>', { delay: 300 })
+  // .delete(5, { delay: 300 })
   .type('<strong class="home__title-color">   최소영</strong>입니다!')
   .go();
